1ecee522b6982db8db28022bd893c720fbab5f82
sushi-chaaaan <mail@sushichan.live>
2022-12-17 13:13:01 +0900

    importの位置を変更

--- python_version_checker/__init__.py
+++ python_version_checker/__init__.py
@@ -1,16 +1,13 @@
-def python_ver_is_over(major: int, minor: int, micro: int) -> bool:
-    import sys
+import sys
 
+
+def python_ver_is_over(major: int, minor: int, micro: int) -> bool:
     return sys.version_info >= (major, minor, micro)
 
 
 def python_ver_is(major: int, minor: int, micro: int) -> bool:
-    import sys
-
     return sys.version_info == (major, minor, micro)
 
 
 def python_ver_is_under(major: int, minor: int, micro: int) -> bool:
-    import sys
-
     return sys.version_info <= (major, minor, micro)
