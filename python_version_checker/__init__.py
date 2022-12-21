40605f4894aa6a8da4cd51e945d90ba66f7e99f0
sushi-chaaaan <mail@sushichan.live>
2022-12-22 01:28:03 +0900

    より詳細なバージョンチェックに対応

--- python_version_checker/__init__.py
+++ python_version_checker/__init__.py
@@ -1,13 +1,74 @@
 import sys
 
 
-def python_ver_is_over(major: int, minor: int, micro: int) -> bool:
-    return sys.version_info >= (major, minor, micro)
+def python_ver_is_over(
+    major: int, minor: int | None = None, micro: int | None = None
+) -> bool:
+    if minor is None:
+        return sys.version_info[0] > major
+    elif micro is None:
+        return sys.version_info[0] > major or sys.version_info[1] > minor
+    else:
+        return (
+            sys.version_info[0] > major
+            or sys.version_info[1] > minor
+            or sys.version_info[2] > micro
+        )
 
 
-def python_ver_is(major: int, minor: int, micro: int) -> bool:
-    return sys.version_info == (major, minor, micro)
+def python_ver_is_over_or_eq(
+    major: int, minor: int | None = None, micro: int | None = None
+) -> bool:
+    if minor is None:
+        return sys.version_info[0] >= major
+    elif micro is None:
+        return sys.version_info[0] >= major or sys.version_info[1] >= minor
+    else:
+        return (
+            sys.version_info[0] >= major
+            or sys.version_info[1] >= minor
+            or sys.version_info[2] >= micro
+        )
 
 
-def python_ver_is_under(major: int, minor: int, micro: int) -> bool:
-    return sys.version_info <= (major, minor, micro)
+def python_ver_is(
+    major: int, minor: int | None = None, micro: int | None = None
+) -> bool:
+    if minor is None:
+        return sys.version_info[0] == major
+    elif micro is None:
+        return sys.version_info[0] == major and sys.version_info[1] == minor
+    else:
+        return (
+            sys.version_info[0] == major
+            and sys.version_info[1] == minor
+            and sys.version_info[2] == micro
+        )
+
+
+def python_ver_is_under(
+    major: int, minor: int | None = None, micro: int | None = None
+) -> bool:
+    if minor is None:
+        return sys.version_info[0] < major
+    elif micro is None:
+        return sys.version_info[0] < major or sys.version_info[1] < minor
+    else:
+        return (
+            sys.version_info[0] < major
+            or sys.version_info[1] < minor
+            or sys.version_info[2] < micro
+        )
+
+
+def python_ver_is_under_or_eq(major: int, minor: int, micro: int) -> bool:
+    if minor is None:
+        return sys.version_info[0] <= major
+    elif micro is None:
+        return sys.version_info[0] <= major or sys.version_info[1] <= minor
+    else:
+        return (
+            sys.version_info[0] <= major
+            or sys.version_info[1] <= minor
+            or sys.version_info[2] <= micro
+        )
