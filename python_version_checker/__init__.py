6160d261d18465ca7649dd900e774368e61a33fe
sushi-chaaaan <mail@sushichan.live>
2022-12-25 13:20:05 +0900

    add docstrings

--- python_version_checker/__init__.py
+++ python_version_checker/__init__.py
@@ -1,74 +1,123 @@
 import sys
 
+from typing import Optional
 
-def python_ver_is_over(
-    major: int, minor: int | None = None, micro: int | None = None
-) -> bool:
+
+def python_ver_is_over(major: int, minor: Optional[int] = None, micro: Optional[int] = None) -> bool:
+    """
+    Checks if the Python version is greater than the one specified in the argument.
+
+    if the minor version is not specified, it checks only the major version.
+
+    if the micro version is not specified, it checks only the major and minor versions.
+
+    Args:
+        major (:obj:`int`): the major version number
+        minor (:obj:`Optional[int]`): the minor version number. Defaults to None.
+        micro (:obj:`Optional[int]`): the micro version number. Defaults to None.
+
+    Returns:
+        bool: `True` if the Python version is greater than the one specified in the argument.
+    """
     if minor is None:
         return sys.version_info[0] > major
     elif micro is None:
         return sys.version_info[0] > major or sys.version_info[1] > minor
     else:
-        return (
-            sys.version_info[0] > major
-            or sys.version_info[1] > minor
-            or sys.version_info[2] > micro
-        )
+        return sys.version_info[0] > major or sys.version_info[1] > minor or sys.version_info[2] > micro
 
 
-def python_ver_is_over_or_eq(
-    major: int, minor: int | None = None, micro: int | None = None
-) -> bool:
+def python_ver_is_over_or_eq(major: int, minor: Optional[int] = None, micro: Optional[int] = None) -> bool:
+    """
+    Checks if the Python version is greater than or equal to the one specified in the argument.
+
+    if the minor version is not specified, it checks only the major version.
+
+    if the micro version is not specified, it checks only the major and minor versions.
+
+    Args:
+        major (:obj:`int`): the major version number
+        minor (:obj:`Optional[int]`): the minor version number. Defaults to None.
+        micro (:obj:`Optional[int]`): the micro version number. Defaults to None.
+
+    Returns:
+        bool: `True` if the Python version is greater than or equal to the one specified in the argument.
+    """
     if minor is None:
         return sys.version_info[0] >= major
     elif micro is None:
         return sys.version_info[0] >= major or sys.version_info[1] >= minor
     else:
-        return (
-            sys.version_info[0] >= major
-            or sys.version_info[1] >= minor
-            or sys.version_info[2] >= micro
-        )
+        return sys.version_info[0] >= major or sys.version_info[1] >= minor or sys.version_info[2] >= micro
 
 
-def python_ver_is(
-    major: int, minor: int | None = None, micro: int | None = None
-) -> bool:
+def python_ver_is(major: int, minor: Optional[int] = None, micro: Optional[int] = None) -> bool:
+    """
+    Checks if the Python version is equal to the one specified in the argument.
+
+    if the minor version is not specified, it checks only the major version.
+
+    if the micro version is not specified, it checks only the major and minor versions.
+
+    Args:
+        major (:obj:`int`): the major version number
+        minor (:obj:`Optional[int]`): the minor version number. Defaults to None.
+        micro (:obj:`Optional[int]`): the micro version number. Defaults to None.
+
+    Returns:
+        bool: `True` if the Python version is equal to the one specified in the argument.
+    """
     if minor is None:
         return sys.version_info[0] == major
     elif micro is None:
         return sys.version_info[0] == major and sys.version_info[1] == minor
     else:
-        return (
-            sys.version_info[0] == major
-            and sys.version_info[1] == minor
-            and sys.version_info[2] == micro
-        )
+        return sys.version_info[0] == major and sys.version_info[1] == minor and sys.version_info[2] == micro
 
 
-def python_ver_is_under(
-    major: int, minor: int | None = None, micro: int | None = None
-) -> bool:
+def python_ver_is_under(major: int, minor: Optional[int] = None, micro: Optional[int] = None) -> bool:
+    """
+    Checks if the Python version is less than the one specified in the argument.
+
+    if the minor version is not specified, it checks only the major version.
+
+    if the micro version is not specified, it checks only the major and minor versions.
+
+    Args:
+        major (:obj:`int`): the major version number
+        minor (:obj:`Optional[int]`): the minor version number. Defaults to None.
+        micro (:obj:`Optional[int]`): the micro version number. Defaults to None.
+
+    Returns:
+        bool: `True` if the Python version is less than the one specified in the argument.
+    """
     if minor is None:
         return sys.version_info[0] < major
     elif micro is None:
         return sys.version_info[0] < major or sys.version_info[1] < minor
     else:
-        return (
-            sys.version_info[0] < major
-            or sys.version_info[1] < minor
-            or sys.version_info[2] < micro
-        )
+        return sys.version_info[0] < major or sys.version_info[1] < minor or sys.version_info[2] < micro
 
 
-def python_ver_is_under_or_eq(major: int, minor: int, micro: int) -> bool:
+def python_ver_is_under_or_eq(major: int, minor: Optional[int] = None, micro: Optional[int] = None) -> bool:
+    """
+    Checks if the Python version is less than or equal to the one specified in the argument.
+
+    if the minor version is not specified, it checks only the major version.
+
+    if the micro version is not specified, it checks only the major and minor versions.
+
+    Args:
+        major (:obj:`int`): the major version number
+        minor (:obj:`Optional[int]`): the minor version number. Defaults to None.
+        micro (:obj:`Optional[int]`): the micro version number. Defaults to None.
+
+    Returns:
+        bool: `True` if the Python version is less than or equal to the one specified in the argument.
+    """
     if minor is None:
         return sys.version_info[0] <= major
     elif micro is None:
         return sys.version_info[0] <= major or sys.version_info[1] <= minor
     else:
-        return (
-            sys.version_info[0] <= major
-            or sys.version_info[1] <= minor
-            or sys.version_info[2] <= micro
-        )
+        return sys.version_info[0] <= major or sys.version_info[1] <= minor or sys.version_info[2] <= micro
